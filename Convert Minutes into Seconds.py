65d139496bfb5f940fdbda812c6036eeb94ed1c2
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2020-09-26 21:25:27 +0300

    Add files via upload

---
+++ Convert Minutes into Seconds.py
@@ -0,0 +1,5 @@
+def convert(minutes):
+
+    return f"{minutes} minutes is {minutes * 60} seconds"
+
+print(convert(5))
---
+++ ConvertHours_IntoSeconds.py
@@ -0,0 +1,9 @@
+def hour_to_sec(hr):
+
+    sec = 3600
+
+    convert = hr * sec
+    print(f"{hr} hours is {convert} seconds")
+
+hour_to_sec(2)
+
