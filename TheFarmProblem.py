53dc6e3ca20115f2845239e7097ac04125c6aaf8
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2020-09-29 14:44:41 +0300

    Create TheFarmProblem.py

---
+++ TheFarmProblem.py
@@ -0,0 +1,14 @@
+"""
+In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+
+chickens = 2 legs
+cows = 4 legs
+pigs = 4 legs
+returns the total number of legs of all the animals.
+"""
+def animals(chickens, cows, pigs):
+
+	return ((chickens * 2) + (cows * 4) + (pigs * 4) )
+
+
+animals(4,3,7)
