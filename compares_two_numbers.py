45a16fc6589a86ed9c96992689ce566d5ead7b83
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2020-10-27 12:47:04 +0300

    Create compares_two_numbers.py
    This function compares two numbers and returns them in increasing order.

---
+++ compares_two_numbers.py
@@ -0,0 +1,10 @@
+# This function compares two numbers and returns them
+# in increasing order.
+def order_numbers(number1, number2):
+	if number2 > number1:
+		return number1, number2
+	else:
+		return number2, number1
+
+smaller,bigger= order_numbers(100, 99)
+print(smaller, bigger)
