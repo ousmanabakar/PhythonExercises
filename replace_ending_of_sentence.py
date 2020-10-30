9ae9d7163d7f3fe2d623fe771d68b1c1146f61aa
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2020-10-30 16:18:00 +0300

    Create replace_ending_of_sentence.py

---
+++ replace_ending_of_sentence.py
@@ -0,0 +1,21 @@
+def replace_ending(sentence, old, new):
+	# Check if the old string is at the end of the sentence 
+	if sentence.split()[-1]== old:
+		
+		i = len(old)
+		new_sentence = sentence[:-i] + new
+		return new_sentence
+
+	return sentence
+	
+print(replace_ending("It's raining cats and cats", "cats", "dogs")) 
+# Should display "It's raining cats and dogs"
+print(replace_ending("She sells seashells by the seashore", "seashells", "donuts")) 
+# Should display "She sells seashells by the seashore"
+print(replace_ending("The weather is nice in May", "may", "april")) 
+# Should display "The weather is nice in May"
+print(replace_ending("The weather is nice in May", "May", "April")) 
+# Should display "The weather is nice in April"
+
+
+
